c03f530b7a43698660d033d08a2a01f353046465
Mike Hoskins <mrh@deadlysyn.com>
2019-07-06 00:20:53 -0400

    build your own Math.min

---
+++ 2-minimum.js
@@ -0,0 +1,9 @@
+// write function emulating Math.min
+
+const min = (x, y) => {
+  return x < y ? x : y
+}
+
+console.log(min(1, 100))  // 1
+console.log(min(0, 5))    // 0
+console.log(min(-10, 10)) // -10
