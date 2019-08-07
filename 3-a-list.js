fdb0847945de6339d5416ebee44fd343bee46219
Mike Hoskins <mrh@deadlysyn.com>
2019-08-06 23:36:19 -0400

    initial commit

---
+++ 3-a-list.js
@@ -0,0 +1,45 @@
+// Build nested list from an array
+const arrayToList = (array) => {
+  let list = {}
+  for (let val of array.reverse()) {
+    if (list.rest === undefined) {
+      list = {
+        value: val,
+        rest: null
+      }
+    } else {
+      list = {
+        value: val,
+        rest: list
+      }
+    }
+  }
+  return list
+}
+
+console.log(arrayToList([1,2,3])) // { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }
+
+// produce an array from a nested list...
+const listToArray = (list) => {
+  let array = []
+  while (true) {
+    array.push(list.value)
+    if (list.rest === null) {
+      break
+    }
+    list = list.rest
+  }
+  return array
+}
+
+console.log(listToArray(arrayToList([1,2,3]))) // [1,2,3]
+
+// prepend an item to a nested list
+const prepend = (val, list) => {
+  return {
+    value: val,
+    rest: list
+  }
+}
+
+console.log(listToArray(prepend(0, (arrayToList([1,2,3]))))) // [0,1,2,3]
